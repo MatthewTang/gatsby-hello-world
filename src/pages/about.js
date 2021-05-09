c48053b2a263a6e57a6f7c54de7e0fb6d6da36f0
Matthew Tang <tshcat@connect.hku.hk>
2021-05-09 16:06:03 +0800

    init

---
+++ src/pages/about.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function About() {
+	return <p>Hi</p>
+}
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Test = () => <h1>hi</h1>
+
+export default function Home() {
+	return (
+		<div style={{ color: "purple" }}>
+			<Link to="/about/">about</Link>
+			<h1>Hello Gatsby!</h1>
+			<p>what a world.</p>
+			<Test />
+		</div>
+	)
+}
