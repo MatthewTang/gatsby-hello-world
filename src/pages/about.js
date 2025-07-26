62f9787b790a972ec00ebb0d2da6b299e7a14ffb
Matthew Tang <matthew.tang@connect.hku.hk>
2025-07-27 01:37:32 +0800

    fix build on node 18

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,10 @@ import React from "react"
 import { Link } from "gatsby"
 
 export default function About() {
-	return (
-		<div>
-			<p>about</p>
-			<Link to="/">home</Link>
-		</div>
-	)
+  return (
+    <div>
+      <p>about</p>
+      <Link to="/">home</Link>
+    </div>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,12 @@ import { Link } from "gatsby"
 const Test = () => <h1>hi</h1>
 
 export default function Home() {
-	return (
-		<div style={{ color: "purple" }}>
-			<Link to="/about/">about</Link>
-			<h1>Hello Gatsby!</h1>
-			<p>test build</p>
-			<Test />
-		</div>
-	)
+  return (
+    <div style={{ color: "purple" }}>
+      <Link to="/about/">about</Link>
+      <h1>Hello Gatsby!</h1>
+      <p>test build</p>
+      <Test />
+    </div>
+  )
 }
