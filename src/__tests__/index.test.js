c0a053b0a02f969c7ff0eb312a4d210f8a8694f4
Matthew Tang <matthew.tang@connect.hku.hk>
2025-08-10 13:42:51 +0800

    Add Jest and React Testing Library with initial test

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "../pages/index";
+
+jest.mock("gatsby", () => ({
+  ...jest.requireActual("gatsby"),
+  Link: ({ to, ...rest }) => <a href={to} {...rest} />,
+}));
+
+test("renders Hello Gatsby!", () => {
+  render(<Home />);
+  expect(screen.getByText("Hello Gatsby!")).toBeInTheDocument();
+});
